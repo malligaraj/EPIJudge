0bee23a8bf276b02ad6e4b6277c284aac7f16720
Malligaraj Malleswaran <m.malligaraj@gmail.com>
2024-04-07 11:57:24 +0200

    Add solution for power_x_y

--- epi_judge_python/power_x_y.py
+++ epi_judge_python/power_x_y.py
@@ -1,9 +1,17 @@
 from test_framework import generic_test
 
-
+# Solution help: NeetCode: https://www.youtube.com/watch?v=g9YQyYi4IQQ
 def power(x: float, y: int) -> float:
-    # TODO - you fill in here.
-    return 0.0
+    def my_pow(x, y):
+        if y == 0: return 1
+        if x == 0: return 0
+
+        res = my_pow(x, y // 2)
+        res *= res
+        return x * res if y % 2 else res
+
+    result = my_pow(x, abs(y))
+    return 1 / result if y < 0 else result
 
 
 if __name__ == '__main__':
